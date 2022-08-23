a363a22837edb1f323bc55aa9ab29f266e25404b
BinderAndreas <andreas.binder93@gmail.com>
2022-08-23 17:19:14 +0200

    Different tries with JS

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function settings() {
-    const options = document.querySelector("#options-pop");
-    const el = document.querySelector("#symbol one");
-    el = options.classList.add("visible");
+    const options = document.querySelector(".options-pop");
+    const el = document.querySelector("#open");
+    options.classList.remove("close");
+    options.classList.add("visible");
+    console.log("asdf")
 }
 
 function closeSettings() {
-    const options = document.querySelector("#options-pop");
-    const el = document.querySelector("#symbol two");
-    el = options.classList.remove("visible")
+    const options = document.querySelector(".options-pop");
+    const el = document.querySelector("#close");
+    options.classList.remove("visible")
+    console.log("asdf")
 }
