7e6fff92f2c734ccc5f10b4d8dcb7152aae63616
BinderAndreas <andreas.binder93@gmail.com>
2022-08-24 12:34:16 +0200

    fixed burger and JS

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 function settings() {
     const options = document.querySelector(".options-pop");
-    const el = document.querySelector("#open");
     options.classList.remove("close");
-    options.classList.add("visible");
-    console.log("asdf")
-}
+   
+};
 
 function closeSettings() {
     const options = document.querySelector(".options-pop");
-    const el = document.querySelector("#close");
-    options.classList.remove("visible")
-    console.log("asdf")
-}
+    options.classList.add("close");
+};
+
